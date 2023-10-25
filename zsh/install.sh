f7bb6f3fadd08f78554ff1029ae6f15ebe661141
Maxio <91082344+MaximCosta@users.noreply.github.com>
2023-10-25 17:11:55 +0200

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,8 +3,19 @@
 sudo apt update
 sudo apt upgrade
 sudo apt-get install zsh keychain bat -y
+
+# zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+# nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+
+# atuin
+bash <(curl https://raw.githubusercontent.com/atuinsh/atuin/main/install.sh)
+
+# curl default config
 curl https://raw.githubusercontent.com/cookies-dev/Dotfile/main/zsh/.p10k.zsh -o ~/.p10k.zsh
 curl https://raw.githubusercontent.com/cookies-dev/Dotfile/main/zsh/.zshrc -o ~/.zshrc
+
+# install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
